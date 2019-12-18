75a3a5e0908c5c8307cc8263b821b1d4a43d7c27
Maurycy Zarzycki <maurycy@evidentlycube.com>
2019-12-18 21:38:35 +0100

    Added docs, index file, upped version

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export {Pool, PoolConfig} from './Pool';
+export {FastPool} from './FastPool';
+export {SlowPool} from './SlowPool';
+export {Grid2D, Grid2DConfig} from './Grid2D';
+export {SpatialGrid2D, SpatialGrid2DConfig} from './SpatialGrid2D';
